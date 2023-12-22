bc55db995e52462b52b083fc1f74794c9882995e
Gustavo Da Silva Silveira <112825271+BugaiOF@users.noreply.github.com>
2023-12-22 03:47:17 -0300

    Restart

--- ToDO-list-Rocketseat/src/App.tsx
+++ ToDO-list-Rocketseat/src/App.tsx
@@ -1,22 +1,9 @@
-import { Header } from './components/Header.tsx'
-import { Task } from './components/Task.tsx';
-
-import './Global.module.css'
-
-import styles from './App.module.css';
-
-
-
-
-export function App(){
+function App() {
   return (
     <div>
-      <Header/>
-      <div className={styles.wrapper}>
-          <main>
-            <Task/>
-          </main>
-        </div>
-      </div>
+      
+    </div>
   )
 }
+
+export default App 
