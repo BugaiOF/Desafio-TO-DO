0e201530669fcdb113688d420056218e75effa76
Gustavo Da Silva Silveira <112825271+BugaiOF@users.noreply.github.com>
2023-12-27 17:30:47 -0300

    Finished

--- ToDO-list-Rocketseat/src/App.tsx
+++ ToDO-list-Rocketseat/src/App.tsx
@@ -1,9 +1,71 @@
+import { useEffect, useState } from "react"
 import { Header } from "./Components/Header/Index"
+import { Tasks } from "./Components/Header/Tasks"
+
+const LOCAL_STORAGE_KEY = "todo:saveTasks"
+
+export interface ITask {
+  id: string; 
+  title: string;
+  isCompleted: boolean;
+}
 
 function App() {
+  const [tasks, setTasks] = useState<ITask[]> ([])
+
+  function loadsavedTasks(){
+    const saved = localStorage.getItem(LOCAL_STORAGE_KEY)
+    if (saved){
+      setTasks(JSON.parse(saved))
+    }
+  }
+
+  useEffect(() => {
+    loadsavedTasks()
+  } , [])
+
+  function setTasksAndSave (newTasks: ITask[]) {  
+    setTasks(newTasks)
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newTasks))
+  }
+
+  function addTask(taskTitle:string,){
+    setTasksAndSave([
+      ...tasks,
+      {
+        id: crypto.randomUUID(),
+        title: taskTitle,
+        isCompleted: false,
+      }
+    ]);
+  }
+
+  function deleteTaskById (taskId :string){
+    const newTasks = tasks.filter((task) => task.id != taskId);
+    setTasksAndSave(newTasks);
+  }
+
+
+  function toggleTaskCompletedById (taskId :string){
+    const newTasks = tasks.map(task => {
+      if(task.id === taskId){
+        return {
+          ...task,
+          isCompleted:  ! task.isCompleted,
+        };
+      }
+      return task;
+    } )
+    setTasksAndSave (newTasks)
+  }
   return (
     <>
-      <Header/>
+      <Header onAddTask = {addTask}/>
+      <Tasks 
+      tasks={tasks} 
+      onDelete={deleteTaskById}
+      onComplete={toggleTaskCompletedById}
+      />
     </>
   )
 }
--- ToDO-list-Rocketseat/src/Components/Header/Index.tsx
+++ ToDO-list-Rocketseat/src/Components/Header/Index.tsx
@@ -1,16 +1,43 @@
 import todoLogo  from '../../assets/Logo.svg'
+import { AiOutlinePlusCircle } from 'react-icons/ai'
 
 import styles from './Header.module.css'
+import { ChangeEvent, FormEvent, useState } from 'react'
 
-export function Header () {
+interface Props {
+    onAddTask: (taskTitle:string) => void
+}
+
+export function Header ({ onAddTask }: Props) {
+    const [title, setTitle] = useState("");
+
+    function handleSubmit(event:FormEvent){
+        event.preventDefault()
+
+        onAddTask(title)
+        setTitle ("")
+    }
+
+    function onChangeTitle(event: ChangeEvent<HTMLInputElement>) {
+        setTitle(event.target.value);
+    }
+
     return (
         <header className={styles.header}>
             <img src={todoLogo}/>
 
 
-            <form className={styles.newTaskForm}>
-                <input placeholder='Adicione uma nova tarefa' />
-                <button>Criar</button>
+            <form className={styles.newTaskForm} onSubmit={handleSubmit}>
+                <input 
+                placeholder='Adicione uma nova tarefa' 
+                onChange={onChangeTitle} 
+                value={title}
+                />
+
+                <button>
+                    Criar 
+                     <AiOutlinePlusCircle size={20}/>    
+                </button>
             </form>
         </header>
     )
